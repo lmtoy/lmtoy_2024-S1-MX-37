a8a7003597a6d8f2a7bf9eadb4aa37565655c164
Peter Teuben <teuben@gmail.com>
2024-08-25 13:48:05 +0000

    upd

--- mk_runs.py
+++ mk_runs.py
@@ -6,7 +6,7 @@ import sys
 
 from lmtoy import runs
 
-project="2023-S1-US-00"
+project="2024-S1-MX-37"
 
 #        obsnums per source (make it negative if not added to the final combination)
 on = {}
@@ -20,21 +20,33 @@ on["L1157-B1"] = [ 119612, 119613, 119615, 119616, 119618, 119619, 119621, 11962
                    119889, 119890, 119896, 119897, 119899, 119900, 119902, 119903,
                    120222, 120223, 120225, 120226, 120228, 120229, 120231, 120232,
                    120234, 120235, 120237, 120238, 120240, 120241,]
+		   
+
+on["L1157-B1_SB0"] = [ 120673, 120674, 120810, 120811,]
+
+on["L1157-B1_SB1"] = [ 120670, 120671, 120807, 120808,]
+
 
 
 #        common parameters per source on the first dryrun (run1a, run2a)
 pars1 = {}
-pars1["L1157-B1"] = "bank=1 extent=240 b_order=1 pix_list=-15 dv=200 dw=200"
+pars1["L1157-B1"]     = "extent=240 b_order=1 pix_list=-15 dv=200 dw=200"
+pars1["L1157-B1_SB0"] = "extent=240 b_order=1 pix_list=-15 dv=200 dw=200"
+pars1["L1157-B1_SB1"] = "extent=240 b_order=1 pix_list=-15 dv=200 dw=200"
 
 
 #        common parameters per source on subsequent runs (run1b, run2b), e.g. bank=0 for WARES
 pars2 = {}
-pars2["L1157-B1"] = "bank=0 pix_list=-0,1,4,5,10,11,12,13 birdies=2045,2048,2051,3069,3072,3075"
+pars2["L1157-B1"]     = "bank=0 pix_list=-0,1,4,5,10,11,12,13 birdies=2045,2048,2051,3069,3072,3075"
+pars2["L1157-B1_SB0"] = "bank=0 pix_list=-0,1,4,5,10,11,12,13 birdies=2045,2048,2051,3069,3072,3075"
+pars2["L1157-B1_SB0"] = "bank=0 pix_list=-0,1,4,5,10,11,12,13 birdies=2045,2048,2051,3069,3072,3075"
 
 #        common parameters per source on subsequent runs (run1c, run2c), e.g. bank=1 for WARES
 pars3 = {}
 
-pars3["L1157-B1"] = "bank=1 birdies=2045,2048,2051,2176,2258,2261,2264,2433,2471,2474,2477,2643,2646,2649,2687,2856,2859,2862,3069,3072,3075 pix_list=-3,8,13,15"
+pars3["L1157-B1"]     = "bank=1 birdies=2045,2048,2051,2176,2258,2261,2264,2433,2471,2474,2477,2643,2646,2649,2687,2856,2859,2862,3069,3072,3075 pix_list=-3,8,13,15"
+pars3["L1157-B1_SB0"] = "bank=1 birdies=2045,2048,2051,2176,2258,2261,2264,2433,2471,2474,2477,2643,2646,2649,2687,2856,2859,2862,3069,3072,3075 pix_list=-3,8,13,15"
+pars3["L1157-B1_SB1"] = "bank=1 birdies=2045,2048,2051,2176,2258,2261,2264,2433,2471,2474,2477,2643,2646,2649,2687,2856,2859,2862,3069,3072,3075 pix_list=-3,8,13,15"
 
 
 
